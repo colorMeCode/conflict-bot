191a34be941794bc8d5191309b1104bf14f3a2a5
Andy Lin <andyzanli@gmail.com>
2023-09-27 23:10:13 -0700

    u

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ async function createConflictComment({
           (file) => file.filename === fileName
         ).blob_url;
 
-        conflictMessage += `\u00A0 - <strong><a href="${blobUrl}">${fileName}</a>:</strong> ${lineNumbers.join(
+        conflictMessage += `- <strong><a href="${blobUrl}">${fileName}</a>:</strong> ${lineNumbers.join(
           ", "
         )}<br />`;
       }
