7a9f4babd92825cb08963cb55f64e321c4e2e554
Andy Lin <andyzanli@gmail.com>
2023-09-27 23:29:43 -0700

    u

--- index.js
+++ index.js
@@ -207,6 +207,7 @@ async function attemptMerge(pr1, pr2) {
     // Fetch PR branches into temporary refs
     execSync(`git fetch origin ${pr1}:refs/remotes/origin/tmp_${pr1}`); // 3
     execSync(`git fetch origin ${pr2}:refs/remotes/origin/tmp_${pr2}`); // 4
+    // execSync(`git fetch origin ${pr1}:${pr1}`); // 3
 
     // Merge main into PR1 in memory
     execSync(`git checkout refs/remotes/origin/tmp_${pr1}`);
